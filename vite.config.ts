e3615e7cd3e4ed6309ed20229ab76506312c69dd
Sanjana P <sanjanapsuresh@gmail.com>
2025-11-07 11:20:11 +0530

    Fix: cleaned merge markers and resolved vite.config.ts build error

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
+
   base: '/Nomadly_travel-Itinerary-API-/',
-=======
->>>>>>> 0817b53 (Initial commit)
+  
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
 });
+
